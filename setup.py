c2e2449c73341fbd86e13bde1c1d07ba738ebf35
Kimberly Pennington <email@kimberly.codes>
2017-06-14 15:19:14 -0600

    edited versions

--- setup.py
+++ setup.py
@@ -10,16 +10,19 @@ def readme():
         return f.read()
 
 setup(name='axe_selenium_python',
-    version='0.1',
+    version='0.0.1',
     description='Python library to integrate axe and selenium for web \
                 accessibility testing.',
     long_description=readme(),
     url='http://github.com/kimberlythegeek/axe_selenium_python',
     author='Kimberly Pennington',
-    license='MPL',
+    author_email='kpennington@mozilla.com',
+    license='Mozilla Public License 2.0 (MPL 2.0)',
     packages=['axe_selenium_python'],
     install_requires=[
-        'pytest-selenium',
-        'pytest'
+        'pytest-selenium>=1.10.0',
+        'pytest>=3.1.1'
     ],
+    test_suite='nose.collector',
+    tests_require=['nose'],
     zip_safe=False)
