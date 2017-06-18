567925c5365ed3aec926d55cb0484d76ab6f4a54
Kimberly Pennington <email@kimberly.codes>
2017-06-18 15:11:08 -0600

    added constructor to Axe. implemented execute() method

--- axe_selenium_python/axe.py
+++ axe_selenium_python/axe.py
@@ -3,45 +3,69 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 
+import selenium
+import time
+
 class Axe:
 
-    def get_contents(self, script_url):
-        """
-        Return contents of the axe.js or axe.min.js script.
+    def __init__(self, selenium, script_url):
+        self.selenium = selenium
+        self.script_url = script_url
+        self.inject(selenium, script_url)
 
-        :param script_url: URL of the axe-core script.
-        :type script_url: string
-        :return: contents of script.
-        :rtype: string
-        """
-        with open(script_url, 'r') as file:
-            script = file.read().replace('\n', '')
-        return script
-
-    def inject(driver, script_url):
+    def inject(self, selenium, script_url):
         """
         Recursively inject aXe into all iframes and the top level document.
 
-        :param driver: WebDriver instance to inject script into.
-        :param script_url: URL of the axe-core script.
-        :type driver: WebDriver
+        :param script_url: location of the axe-core script.
         :type script_url: string
         """
+        selenium.execute_script(open(script_url).read())
 
-    def inject_into_frames(driver, script, parents):
-        """
-        Recursively find frames and inject a script into them.
+    def execute(self, selenium):
+        command = 'return axe.run().then(function(result){return result;});'
+        response = selenium.execute_script(command)
+        return response
 
-        :param driver: WebDriver instance to inject script into.
-        :param script: Script to inject.
-        :type driver: WebDriver
-        :type script: string
-        """
+    # Skip iframes for now!
+    # def inject_into_frames(self, script, parents, selenium):
+    #     """
+    #     Recursively find frames and inject a script into them.
+    #
+    #     :param script: Script to inject.
+    #     :param parents: list of parent elements
+    #     :type script: string
+    #     :type parents: list of WebElements
+    #     """
+    #     # Find all iframes within current document or iframe
+    #     frames = selenium.find_elements_by_tag_name('iframe')
+    #     # For each frame
+    #     for frame in frames:
+    #         # Switch to top level element (current frame)
+    #         selenium.switch_to.default_content
+    #
+    #         # If parents list exists
+    #         if (parents is not None):
+    #             # for each parent
+    #             for parent in parents:
+    #                 # Focus the parent
+    #                 selenium.switch_to.frame(parent)
+    #                 # (Selenium can't select iframes within iframes)
+    #                 # This focuses the innermost frame this iteration
+    #
+    #         # Focus next frame and inject javascript
+    #         selenium.switch_to.frame(frame)
+    #         selenium.execute_script(script)
+    #
+    #         # Make a local copy of parents list
+    #         local_parents = list(parents)
+    #         # Add current frame as a parent for next iteration
+    #         local_parents.append(frame)
+    #         # Recurse and search for frames within current frame
+    #         self.inject_into_frames(script, local_parents)
 
     def report(violations):
         """
-        ??? Do we want this functionality in our package?
-
         Return readable report of accessibility violations found.
 
         :param violations: Dictionary of violations.
@@ -57,67 +81,3 @@ class Axe:
         :param name: Name of file to be written to.
         :param output: JSON object.
         """
-
-    class Builder:
-        """
-        Chainable builder for invoking aXe. Instantiate a new Builder and
-        configure testing with the include(), exclude(), and options() methods
-        before calling analyze() to run.
-
-        driver (WebDriver)
-        script (string): URL of script
-        includes (list): list of selectors to include.
-        excludes (list): list of selectors to exclude.
-        options (dict): Dictionary of aXe configuration options.
-        """
-
-        def __init__(driver, script):
-            """
-            Inject the aXe script into the WebDriver.
-            :param driver: An intialized WebDriver.
-            :param script: URL of aXe script.
-            :type driver: WebDriver
-            :type script: string
-            """
-
-        def options(options):
-            """
-            Set the aXe options.
-
-            :param options: Dictionary of aXe configuration options.
-            :type options: dict
-            """
-
-        def include(selector):
-            """
-            Include a selector.
-
-            :param selector: any valid CSS selector.
-            :type selector: string
-            """
-
-        def exclude(selector):
-            """
-            Exclude a selector.
-
-            :param selector: any valid CSS selector.
-            :type selector: string
-            """
-
-        def analyze():
-            """
-            Run aXe against the page.
-
-            :return results: Dictionary of JSON results.
-            :rtype results: dict
-            """
-
-        def analyze_element(context):
-            """
-            Rune aXe against a specific WebElement.
-
-            :param context: Web element to be analyzed.
-            :type context: WebElement
-            :return results: aXe JSON results
-            :rtype: dict
-            """
