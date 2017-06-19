d558ac77e79fd7213d0bec71f168d9c24ce693d9
Kimberly Pennington <email@kimberly.codes>
2017-06-19 13:33:07 -0600

    removed recursive iframe injection

--- axe_selenium_python/axe.py
+++ axe_selenium_python/axe.py
@@ -2,7 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-
+import json
 import selenium
 import time
 
@@ -27,43 +27,6 @@ class Axe:
         response = selenium.execute_script(command)
         return response
 
-    # Skip iframes for now!
-    # def inject_into_frames(self, script, parents, selenium):
-    #     """
-    #     Recursively find frames and inject a script into them.
-    #
-    #     :param script: Script to inject.
-    #     :param parents: list of parent elements
-    #     :type script: string
-    #     :type parents: list of WebElements
-    #     """
-    #     # Find all iframes within current document or iframe
-    #     frames = selenium.find_elements_by_tag_name('iframe')
-    #     # For each frame
-    #     for frame in frames:
-    #         # Switch to top level element (current frame)
-    #         selenium.switch_to.default_content
-    #
-    #         # If parents list exists
-    #         if (parents is not None):
-    #             # for each parent
-    #             for parent in parents:
-    #                 # Focus the parent
-    #                 selenium.switch_to.frame(parent)
-    #                 # (Selenium can't select iframes within iframes)
-    #                 # This focuses the innermost frame this iteration
-    #
-    #         # Focus next frame and inject javascript
-    #         selenium.switch_to.frame(frame)
-    #         selenium.execute_script(script)
-    #
-    #         # Make a local copy of parents list
-    #         local_parents = list(parents)
-    #         # Add current frame as a parent for next iteration
-    #         local_parents.append(frame)
-    #         # Recurse and search for frames within current frame
-    #         self.inject_into_frames(script, local_parents)
-
     def report(self, violations):
         """
         Return readable report of accessibility violations found.
@@ -73,10 +36,22 @@ class Axe:
         :return report: Readable report of violations.
         :rtype: string
         """
+        global string
         string = ''
         string += 'Found ' + str(len(violations)) + ' accessibility violations:'
-        for index, item in enumerate(violations):
-            string += '\n' + str(index + 1) + ') ' + violations[item]['help']
+        for i, item in enumerate(violations):
+            string += '\n' + str(i + 1) + ') ' + violations[item]['help']
+            if violations[item]['helpUrl'] is not None:
+                string += ': ' + violations[item]['helpUrl'] + '\n\n'
+
+            nodes = violations[item]['nodes']
+            for j, node in enumerate(nodes):
+                for target in node['target']:
+                    string += target + '\t'
+
+                string += '\n\n' + node['failureSummary'] + '\n\n'
+                string += 'Impact: ' + node['impact'] + '\n'
+
         return string
 
     def write_results(name, output):
