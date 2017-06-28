f4cb645e1be6b5c94e57f4d0ff580f049d3ad3df
Kimberly Pennington <email@kimberly.codes>
2017-06-28 14:19:53 -0700

    remove test suite settings (use tox instead)

--- setup.py
+++ setup.py
@@ -6,23 +6,20 @@
 from setuptools import setup
 
 def readme():
-    with open('README.rst') as f:
+    with open('./README.rst') as f:
         return f.read()
 
 setup(name='axe_selenium_python',
-    version='0.0.1',
+    version='0.0.2',
     description='Python library to integrate axe and selenium for web \
                 accessibility testing.',
     long_description=readme(),
     url='http://github.com/kimberlythegeek/axe_selenium_python',
     author='Kimberly Pennington',
     author_email='kpennington@mozilla.com',
-    license='Mozilla Public License 2.0 (MPL 2.0)',
     packages=['axe_selenium_python'],
     install_requires=[
-        'pytest-selenium>=1.10.0',
-        'pytest>=3.1.1'
+        'pytest-selenium>=1.10.0'
     ],
-    test_suite='nose.collector',
-    tests_require=['nose'],
-    zip_safe=False)
+    license='Mozilla Public License 2.0 (MPL 2.0)',
+    keywords='axe-core selenium pytest-selenium accessibility automation mozilla')
