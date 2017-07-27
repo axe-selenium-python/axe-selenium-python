539d64cdbd32b7a9903dc813d8cce25f5e398ec2
Kimberly Pennington <email@kimberly.codes>
2017-07-27 14:04:30 -0600

    version 0.0.2 to 0.0.21

--- setup.py
+++ setup.py
@@ -7,10 +7,13 @@ from setuptools import setup
 
 def readme():
     with open('./README.rst') as f:
-        return f.read()
+        readme = f.read()
+    with open('./CHANGELOG.rst') as f:
+        log = f.read()
+    return readme + '\n\n' + log
 
 setup(name='axe-selenium-python',
-    version='0.0.2',
+    version='0.0.21',
     description='Python library to integrate axe and selenium for web \
                 accessibility testing.',
     long_description=readme(),
