a483acc5b9f59a472af0f494a8ce182250ff2e75
Kimberly Pennington <email@kimberly.codes>
2017-09-04 09:45:07 -0600

    updated version to 1.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def readme():
 
 
 setup(name='axe-selenium-python',
-      version='0.0.450',
+      version='1.0.000',
       description='Python library to integrate axe and selenium for web \
                 accessibility testing.',
       long_description=readme(),
