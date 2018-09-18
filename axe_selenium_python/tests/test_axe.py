ba24ec56528e4458f0a5604a8d511923c943a479
Kimberly <ksereduck@mozilla.com>
2018-09-18 11:01:39 -0600

    * make it black * update readme to remove docker reference

--- axe_selenium_python/tests/test_axe.py
+++ axe_selenium_python/tests/test_axe.py
@@ -9,12 +9,12 @@ from selenium import webdriver
 
 from ..axe import Axe
 
-_DEFAULT_TEST_FILE = path.join(path.dirname(__file__), 'test_page.html')
+_DEFAULT_TEST_FILE = path.join(path.dirname(__file__), "test_page.html")
 
 
-@pytest.fixture(params=('Firefox', pytest.mark.xfail('Chrome', reason="issue #118")))
-def driver(request):
-    driver = getattr(webdriver, request.param)()
+@pytest.fixture
+def driver():
+    driver = webdriver.Firefox()
     yield driver
     driver.close()
 
@@ -22,12 +22,12 @@ def driver(request):
 @pytest.mark.nondestructive
 def test_run_axe_sample_page(driver):
     """Run axe against sample page and verify JSON output is as expected."""
-    driver.get('file://' + _DEFAULT_TEST_FILE)
+    driver.get("file://" + _DEFAULT_TEST_FILE)
     axe = Axe(driver)
     axe.inject()
     data = axe.execute()
 
-    assert len(data["inapplicable"]) == 46
+    assert len(data["inapplicable"]) == 50
     assert len(data["incomplete"]) == 0
     assert len(data["passes"]) == 7
     assert len(data["violations"]) == 8
--- setup.py
+++ setup.py
@@ -5,28 +5,27 @@
 
 from setuptools import find_packages, setup
 
+with open("./README.rst") as f:
+    readme = f.read()
 
-def readme():
-    with open('./README.rst') as f:
-        readme = f.read()
-    with open('./CHANGELOG.rst') as f:
-        log = f.read()
-    return readme + '\n\n' + log
-
-
-setup(name='axe-selenium-python',
-      version='2.0.5',
-      description='Python library to integrate axe and selenium for web \
-                accessibility testing.',
-      long_description=readme(),
-      url='http://github.com/mozilla-services/axe-selenium-python',
-      author='Kimberly Sereduck',
-      author_email='ksereduck@mozilla.com',
-      packages=find_packages(),
-      package_data={'axe_selenium_python': ['src/axe.min.js'], },
-      install_requires=[
-          'selenium>=3.0.2',
-          'pytest>=3.0'
-      ],
-      license='Mozilla Public License 2.0 (MPL 2.0)',
-      keywords='axe-core selenium pytest-selenium accessibility automation mozilla')
+setup(
+    name="axe-selenium-python",
+    use_scm_version=True,
+    description="Python library to integrate axe and selenium for web \
+                accessibility testing.",
+    long_description=open("README.rst").read(),
+    url="http://github.com/mozilla-services/axe-selenium-python",
+    author="Kimberly Sereduck",
+    author_email="ksereduck@mozilla.com",
+    packages=find_packages(),
+    package_data={
+        "axe_selenium_python": [
+            "axe_selenium_python/node_modules/axe-core/axe.min.js",
+            "axe_selenium_python/tests/test_page.html",
+        ]
+    },
+    include_package_data=True,
+    install_requires=["selenium>=3.0.2", "pytest>=3.0"],
+    license="Mozilla Public License 2.0 (MPL 2.0)",
+    keywords="axe-core selenium pytest-selenium accessibility automation mozilla",
+)
